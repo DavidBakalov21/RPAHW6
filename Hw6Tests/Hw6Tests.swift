1ca722a98be5f2b56a5e93424ee56d2748b15415
DavidBakalov21 <davidbakalov33@gmail.com>
2024-12-11 22:09:05 +0200

    Initial Commit

---
+++ Hw6Tests/Hw6Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Hw6Tests.swift
+//  Hw6Tests
+//
+//  Created by david david on 11.12.2024.
+//
+
+import Testing
+@testable import Hw6
+
+struct Hw6Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
